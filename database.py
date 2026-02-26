07aac4204c16c79e30b0955991b999d50eb17862
dinah-parag <dinah.dantass@gmail.com>
2026-02-25 21:19:54 -0300

    org

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ banco = 'desafio_vendas'
 
 engine = create_engine(f'mysql+pymysql://{usuario}:{senha}@{host}/{banco}')
 
-# criação da tabela 'vendas_limpas' no 'desafio_vendas'
+# criação da tabela 'vendas_limpas' no 'desafio_vendas')
 df.to_sql('vendas_limpas', con=engine, if_exists='replace', index=False)
 
 print("Processo concluído: Arquivo lido da pasta 'data' e salvo no MySQL!")
